8cc2fb62b5a95c58bb1e041820cf21e12af2aa94
hxili <hxili@ucdavis.edu>
2023-02-02 13:40:55 -0800

    init

---
+++ 24gc.py
@@ -0,0 +1,15 @@
+# 24gc.py
+
+# Write a program that computes the GC% of a DNA sequence
+# Format the output for 2 decimal places
+
+dna = 'ACAGAGCCAGCAGATATACAGCAGATACTAT'
+GC = 0
+for i in dna:
+    if i == 'C' or i == 'G': GC += 1
+print(f'{GC/len(dna):.2f}')
+
+"""
+python3 24gc.py
+0.42
+"""
